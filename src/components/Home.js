159fd1c37c8456c7a7c7349b1dd8e0976d3495e4
Jackson93150 <jackjack93150@gmail.com>
2022-08-09 21:20:52 +0200

    fix 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,24 +9,24 @@ import Furtif from "../Furtif"
 
 function Home() {
   return (
-    <div className='container'>
-        <div className='home'>
+    <div className='home'>
+        <div className='container'>
             <div className='Title'>
               <img src={title} alt='title' id='title'/>
-              <img src={Ellipse} alt='Ellipse' id='ellipse'/>
-              <img src={Group} alt='Group' id='group'/>
+              <img src={Ellipse} alt='ellipse' id='ellipse'/>
+              <img src={Group} alt='group' id='group'/>
               <img src={palmier} alt='palmier' id='palmier'/>
               <div className='Jacky'>
                 <Canvas camera={{zoom:0.7}}>
-                  <directionalLight intensity={0.5}/>
-                  <ambientLight intensity={0.2}/>
-                  <pointLight position={[30, 10, 20]} intensity={1} />
-                  <Suspense fallback={null}>
-                    <Furtif/>
-                    <Furtif position={[0,0,0]}/>
-                  </Suspense>
+                    <directionalLight intensity={0.5}/>
+                    <ambientLight intensity={0.2}/>
+                    <pointLight position={[30, 10, 20]} intensity={1} />
+                    <Suspense fallback={null}>
+                      <Furtif/>
+                      <Furtif position={[0,0,0]}/>
+                    </Suspense>
                 </Canvas>
-            </div>
+              </div>
             </div>
         </div>
     </div>
