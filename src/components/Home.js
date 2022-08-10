2b98752a2177480603a6c15aded9bd0cf1a7091c
Jackson93150 <jackjack93150@gmail.com>
2022-08-10 12:07:42 +0200

    animate home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,41 @@
-import React, {Suspense} from 'react'
-import "./Home.css"
-import sunset from '../assets/sunset.png'
-import titre from '../assets/titre.png'
-import {Canvas} from "@react-three/fiber"
-import Furtif from "../Furtif"
-import scroll from "../assets/scroll.gif"
+import React, { Suspense } from "react";
+import "./Home.css";
+import sunset from "../assets/sunset.png";
+import titre from "../assets/titre.png";
+import { Canvas } from "@react-three/fiber";
+import Furtif from "../Furtif";
+import scroll from "../assets/scroll.gif";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
-    <div className='home'>
-        <div className='container'>
-            <div className='Title'>
-              <img src={sunset} alt='sunset' id='sunset'/>
-              <img src={titre} alt='titre' id='titre'/>
-              <div className='Jacky'>
-                <Canvas camera={{zoom:0.7}}>
-                    <directionalLight intensity={0.5}/>
-                    <ambientLight intensity={0.2}/>
-                    <pointLight position={[30, 10, 20]} intensity={1} />
-                    <Suspense fallback={null}>
-                      <Furtif/>
-                      <Furtif position={[0,0,0]}/>
-                    </Suspense>
-                </Canvas>
-              </div>
+    <div className="home">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div className="container">
+          <div className="Title">
+            <img src={sunset} alt="sunset" id="sunset" />
+            <img src={titre} alt="titre" id="titre" />
+            <div className="Jacky">
+              <Canvas camera={{ zoom: 0.7 }}>
+                <directionalLight intensity={0.5} />
+                <ambientLight intensity={0.2} />
+                <pointLight position={[30, 10, 20]} intensity={1} />
+                <Suspense fallback={null}>
+                  <Furtif />
+                  <Furtif position={[0, 0, 0]} />
+                </Suspense>
+              </Canvas>
             </div>
-            <img src={scroll} alt='scroll' id='scroll'/>
+          </div>
+          <img src={scroll} alt="scroll" id="scroll" />
         </div>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
