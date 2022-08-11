af27cb4bef5e00781b8149e57c68b6ccfb3ce8b8
Jackson93150 <jackjack93150@gmail.com>
2022-08-11 17:23:31 +0200

    glasses adjustement

--- src/Dance.js
+++ src/Dance.js
@@ -19,7 +19,7 @@ function Model(props) {
           <primitive object={nodes.mixamorigHips} />
           <group name="Aviators_Transparent_0">
             <skinnedMesh name="Aviators_Transparent_0001" geometry={nodes.Aviators_Transparent_0001.geometry} material={materials['Transparent.001']} skeleton={nodes.Aviators_Transparent_0001.skeleton} />
-            <skinnedMesh name="Aviators_Transparent_0001_1" geometry={nodes.Aviators_Transparent_0001_1.geometry} material={materials['Glass_tinted.001']} skeleton={nodes.Aviators_Transparent_0001_1.skeleton} />
+            <skinnedMesh name="Aviators_Transparent_0001_1" geometry={nodes.Aviators_Transparent_0001_1.geometry} material={materials['Glass_tinted.001']} skeleton={nodes.Aviators_Transparent_0001_1.skeleton} transparent opacity={0}/>
             <skinnedMesh name="Aviators_Transparent_0001_2" geometry={nodes.Aviators_Transparent_0001_2.geometry} material={materials['Gold_metallic.001']} skeleton={nodes.Aviators_Transparent_0001_2.skeleton} />
           </group>
           <skinnedMesh name="Cube" geometry={nodes.Cube.geometry} material={materials['Material.004']} skeleton={nodes.Cube.skeleton} />
