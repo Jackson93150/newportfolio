bbc47d4381af29aeba237f071f47f0b8ba2a54c0
Jackson93150 <jackjack93150@gmail.com>
2022-08-11 13:30:58 +0200

    start about fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Home />
     </div>
   );
